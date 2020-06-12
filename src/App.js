b22a4ab7cff2854f03d6cebd737ebafa8bb34575
Zubaidullo <zubaidullo.uulu@g42.ai>
2020-06-12 17:38:08 +0400

    All changes until 57. Outputting list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,80 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState ] = useState({
+class App extends Component  {
+
+  state = {
     persons : [
       {name: 'Max', age:28}, 
       {name: 'Manu', age:29}, 
       {name: 'Stephanie', age:26}
     ],
-    otherState: 'some other value' 
-  })
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-  const swtichNameHandler = () => {
-    // console.log('was clicked');
-    // DON"T DO THIS: this.state.persons[0].name = 'Maximilian';
-    setPersonsState({persons : [
-      {name: 'Maximilian', age:28}, 
+  swtichNameHandler = (newName) => {
+    this.setState({persons : [
+      {name: newName, age:28}, 
       {name: 'Manu', age:29}, 
       {name: 'Stephanie', age:27}
     ] })
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm React App</h1>
-      <p>This is really working!</p>
-      <button onClick={this.swtichNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
-  );
+  nameChangedHandler = (event) => {
+    this.setState({persons : [
+      {name: 'Max', age:28}, 
+      {name: event.target.value, age:29}, 
+      {name: 'Stephanie', age:27}
+    ] })
+  }
+
+  togglerPersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+    if ( this.state.showPersons) {
+      persons = (
+        <div>
+              <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}/>
+              <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}
+                click={this.swtichNameHandler}
+                changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+              <Person 
+                name={this.state.persons[2].name} 
+                age={this.state.persons[2].age}/>
+            </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglerPersonHandler} >Toggle Persons</button>
+        {persons}  
+      </div>
+    )
+  }
 }
 
-export default app;
+export default App;
